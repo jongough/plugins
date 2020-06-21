af3282d91dc0337b021293514fa1df3b0153ce82
jongough <jon.gough@eclipsesystems.com.au>
2020-06-21 18:49:04 +1000

    again

--- validate_xml.sh
+++ validate_xml.sh
@@ -41,7 +41,7 @@ else
                 exit_rc=$rc
             fi
         fi
-    done < <( ${{ steps.getfile.outputs.files1 }} )
+    done < <( git diff-tree --no-commit-id --name-only -r $commit_sha)
     #done < <( git diff --name-only ..master)${{ steps.getfile.outputs.files1 }}
     if [[ $exit_rc == 0 ]]; then
         echo "All files pass git pull xsd check"
