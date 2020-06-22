0e0767e7f5de2807be8edde7f787854475dd94ec
jongough <jon.gough@eclipsesystems.com.au>
2020-06-22 15:56:38 +1000

    again

--- validate_xml.sh
+++ validate_xml.sh
@@ -27,27 +27,12 @@ if [ -z $CI ] && [ -z $GITHUB_ACTION ]; then
     exit $exit_rc
 else
     exit_rc=0
-    #gitdiff="$(`git --no-pager diff --name-only ..HEAD`)"
-    #echo "gitdiff: ${gitdiff}"
-    #gitdifffiles=`$(git diff-tree --no-commit-id --name-only -r ${commit_sha})`
-    #echo "gitdifffiles: ${gitdifffiles}"
-    echo "Files: ${FILES}"
-    echo "commit_sha: ${GITHUB_SHA}"
-    echo "GITHUB_REPOSITORY: ${GITHUB_REPOSITORY}"
-    echo "REQUEST_NO: ${REQUEST_NO}"
-    echo "FILES no: ${#FILES[@]}"
-    IFS=' ' read -r -a array <<< "$FILES"
-    echo "array size: ${#arrray[@]}"
     URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${REQUEST_NO}/files"
     FILES_FOUND=$(curl -s -X GET -G $URL | jq -r '.[] | .filename')
-    files_array_val=($FILES_FOUND)
-    echo "Validate Files: $FILES_FOUND"
-    echo "Files content: $FILES_ARRAY[1]"
-    echo "File_array: $#FILE_ARRAY[@]"
-    echo "files_found_array: ${#files_array_val[@]}"
-    echo "files_array_val[1]: $files_array_val[1]"
-
-    while read -r file; do
+    files_array=($FILES_FOUND)
+    for file in "${files_array[@]}"
+    do
+        echo "Found file: $file"
         if [[ $file == "metadata"*".xml" ]]; then
             echo "Processing file: $file"
             `xmllint  --schema ocpn-plugins.xsd $file --noout 2> /dev/null`
@@ -57,7 +42,19 @@ else
                 exit_rc=$rc
             fi
         fi
-    done < <( $(curl -s -X GET -G $URL | jq -r '.[] | .filename') )
+    done
+
+#    while read -r file; do
+#        if [[ $file == "metadata"*".xml" ]]; then
+#            echo "Processing file: $file"
+#            `xmllint  --schema ocpn-plugins.xsd $file --noout 2> /dev/null`
+#            rc=$?
+#            if [ $rc -gt 0 ]; then
+#                `xmllint  --schema ocpn-plugins.xsd $file --noout`
+#                exit_rc=$rc
+#            fi
+#        fi
+#    done < <( $(curl -s -X GET -G $URL | jq -r '.[] | .filename') )
     #done < <( git diff --name-only ..master)${{ steps.getfile.outputs.files1 }}
     if [[ $exit_rc == 0 ]]; then
         echo "All files pass git pull xsd check"
